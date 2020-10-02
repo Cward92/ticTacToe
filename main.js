2b00f90efed2fe308d82d25a61e2d2b89943492a
Chandler Ward <chandlermward92@gmail.com>
2020-10-02 11:02:59 -0400

    add v0.5 no functions, but board render and state change

--- main.js
+++ main.js
@@ -1,87 +1,80 @@
-//initialize header variables
-let state = 0;
+/*----- state (variables) -----*/
+let turn = 'X';
+let board;
 
 let main = document.getElementById('main');
 let con = document.createElement('div');
-    con.setAttribute('class', 'container');
+con.setAttribute('class', 'container');
+createHeader();
+createBoard();
 
+let squares = Array.from(document.querySelectorAll('#board div'));
+let c_turn = document.getElementById('score');
+
+
+/*----- functions -----*/
+function init() {
+    board = ['y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y'];
+    render();
+    document.getElementById('board').addEventListener('click', handleTurn);
+};
+
+function render() {
+    board.forEach(function (value, index) {
+        squares[index].textContent = value;
+    });
+    c_turn.innerHTML = `Turn: ${turn}`;
+};
+
+/*----- create header -------*/
 function createHeader() {
     var head = document.createElement('div');
     var header = document.createElement('header');
     var div_0 = document.createElement('div');
-    var p_score = document.createElement('p');
     var h1_title = document.createElement('h1');
-    var p_player = document.createElement('p');
+    var p_turn = document.createElement('p');
 
     head.setAttribute('class', 'header');
     head.setAttribute('id', 'header');
     div_0.setAttribute('class', 'm-2 text-center border-bottom');
-    p_score.setAttribute('class', 'pr-3 d-flex justify-content-end');
-    p_score.innerHTML = "Score: ";
     h1_title.innerHTML = "Tic Tac Toe";
-    p_player.setAttribute('class', 'd-flex justify-content-start');
-    p_player.innerHTML = "Turn: ";
+    p_turn.setAttribute('class', 'd-flex justify-content-start');
+    p_turn.setAttribute('id', 'score');
+    p_turn.innerHTML = `Turn: ${turn}`;
 
-    div_0.appendChild(p_score);
     div_0.appendChild(h1_title);
-    div_0.appendChild(p_player);
+    div_0.appendChild(p_turn);
     header.appendChild(div_0);
     head.appendChild(header);
     document.getElementById('main').appendChild(head);
-}
 
-// create main
+};
+
+/*-------- create board ---------*/
 function createBoard() {
     var row = document.createElement('div');
     row.setAttribute('id', 'board');
     row.setAttribute('class', 'row');
     con.appendChild(row);
 
-    var hr = document.createElement('hr');
 
     for (let i = 0; i < 9; i++) {
         var col = document.createElement('div');
-        var text = document.createTextNode("_");
-        col.appendChild(text);
-        col.setAttribute('id', "col_" + i);
-        if (i < 3) {
-            col.setAttribute('class', 'col-4 text-center border border-top-0');
-        } else if (i > 5) {
-            col.setAttribute('class', 'col-4 text-center border border-bottom-0');
-        } else {
-            col.setAttribute('class', 'col-4 text-center border');
-        }
+        col.setAttribute('class', 'col-4 text-center border border-dark');
         row.appendChild(col);
     };
-    con.appendChild(hr);
     main.appendChild(con);
+
 }
 
-function updateBlock() {
-    if (state = 0) {
-        cconsole.log("works");
-    }
-}
+function handleTurn(e) {
+    let index = squares.findIndex(function(squares) {
+        return squares === e.target;
+    });
+    board[index] = turn;
+    turn = turn === 'X' ? 'O'  : 'X';
+    render();
+    
+};
 
-function resetState() {
-    state = 0;
-}
-
-function createButton() {
-    let div = document.createElement('div');
-    div.setAttribute('class', 'btn');
-    div.setAttribute('id', 'button');
-
-    let btn = document.createElement('button');
-    btn.setAttribute('class', 'btn btn-outline-success');
-    btn.setAttribute('onClick', 'resetState()');
-    btn.innerHTML = "Reset";
-    div.appendChild(btn);
-    con.appendChild(div);
-}
-
-
-
-createHeader();
-createBoard();
-createButton();
+init();
