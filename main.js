0cba5d63ed64102a265ac0c8f83e9556fc9475ff
Chandler Ward <chandlermward92@gmail.com>
2020-10-08 12:03:09 -0400

    fixed win message for player 2

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //View
 
 let turn = 'X';
-let arr = ['-', '-', '-', 
-           '-', '-', '-', 
-           '-', '-', '-'];
+let arr = ['-', '-', '-',
+    '-', '-', '-',
+    '-', '-', '-'];
 let movesUsed = 0;
 const movesAllowed = 9;
 let winner = false;
@@ -30,8 +30,8 @@ const winConditions = [
 
 //declare function to generate html elements
 function generateElement(
-    type, id, classList, eventListener=false
-    ) {
+    type, id, classList
+) {
     let newElement = document.createElement(type);
     newElement.id = id;
     newElement.setAttribute('class', classList);
@@ -41,11 +41,11 @@ function generateElement(
 let main = document.getElementById('main'); //main div
 let h_row = generateElement('div', 'head', 'row text-center');
 let title = generateElement('h1', 'title', 'pt-3 col-12');
-    title.innerHTML = 'Tic-Tac-Toe';
+title.innerHTML = 'Tic-Tac-Toe';
 let score1 = generateElement('p', 'player_1_score', 'col-6');
 let score2 = generateElement('p', 'player_2_score', 'col-6');
 let board = generateElement('div', 'board', 'row pl-n3 d-flex justify-content-center');
-for(let i = 0; i < 9; i++) {
+for (let i = 0; i < 9; i++) {
     let newCol = generateElement('div', i, 'col-4 border');
     board.appendChild(newCol);
 };
@@ -66,28 +66,28 @@ init();
 function chooseName() {
     console.log(score1)
     p1name = prompt("Enter Name of Player 1", "");
-    if(p1name == null || p1name == '') {
+    if (p1name == null || p1name == '') {
         p1name = 'Player 1';
-    //     score1.innerHTML = `${p1name}: ${p1score}`;
-    // } else {
+        //     score1.innerHTML = `${p1name}: ${p1score}`;
+        // } else {
     }
     score1.innerHTML = `<span style='color: red;'> ${p1name}: ${p1score}`;
     p2name = prompt("Enter Name of Player 2", "");
-    if(p2name == null || p2name == '') {
+    if (p2name == null || p2name == '') {
         p2name = 'Player 2';
-    //     score2innerHTML = `${p2name}: ${p2score}`;
-    // } else {
+        //     score2innerHTML = `${p2name}: ${p2score}`;
+        // } else {
     }
     score2.innerHTML = `${p2name}: ${p2score}`;
 }
 
 
-function init(){
-    arr = ['-', '-', '-', 
-           '-', '-', '-', 
-           '-', '-', '-'];
+function init() {
+    arr = ['-', '-', '-',
+        '-', '-', '-',
+        '-', '-', '-'];
     let squares = Array.from(document.querySelectorAll('#board div'));
-    for(let i = 0; i < squares.length; i++){
+    for (let i = 0; i < squares.length; i++) {
         squares[i].innerHTML = arr[i];
         squares[i].addEventListener('click', doTurn);
     };
@@ -97,8 +97,8 @@ function init(){
     turn = 'X';
 };
 
-function doTurn(e){
-    if(winner == true){
+function doTurn(e) {
+    if (winner == true) {
         return;
     } else {
         updateGrid(e);
@@ -108,29 +108,29 @@ function doTurn(e){
     };
 };
 
-function updateGrid(e){
+function updateGrid(e) {
     e.target.innerHTML = `${turn}`;
     arr[e.target.id] = `${turn}`;
     e.target.removeEventListener('click', doTurn);
 };
 
-function winCondition(){
+function winCondition() {
     movesUsed++;
-    if(movesUsed > 4) {
+    if (movesUsed > 4) {
         checkWin();
     };
 };
 
 function checkWin() {
-    for(var i = 0; i < winConditions.length; i++) {
+    for (var i = 0; i < winConditions.length; i++) {
         var sum = 0;
         var win = winConditions[i];
-        for(var j = 0; j < win.length; j++) {
-            if(arr[win[j]] === `${turn}`) {
+        for (var j = 0; j < win.length; j++) {
+            if (arr[win[j]] === `${turn}`) {
                 sum++;
             };
         };
-        if(sum === 3) {
+        if (sum === 3) {
             winner = true;
             console.log(winner);
             return winner;
@@ -139,14 +139,14 @@ function checkWin() {
     return winner = false;
 };
 
-function checkWinner(){
+function checkWinner() {
     if (winner == true) {
-        alert(`${p1name} WINS!`);
-        console.log(currentPlayer);
-        if(currentPlayer == 1) {
+        if (currentPlayer == 1) {
+            alert(`${p1name} WINS!`);
             p1score++;
             score1.innerHTML = `${p1name}: ${p1score}`;
-        } else if(currentPlayer == 2){
+        } else if (currentPlayer == 2) {
+            alert(`${p2name} WINS!`);
             p2score++;
             score2.innerHTML = `${p2name}: ${p2score}`;
         }
@@ -157,16 +157,16 @@ function checkWinner(){
 };
 
 function updateTurn() {
-    if(turn == 'X') {
+    if (turn == 'X') {
         turn = 'O';
     } else {
         turn = "X";
     };
-    if(currentPlayer == 1) {
+    if (currentPlayer == 1) {
         currentPlayer = 2;
         score2.innerHTML = `<span style='color: red;'>${p2name}: ${p2score}</span>`;
         score1.innerHTML = `${p1name}: ${p1score}`
-    } else if(currentPlayer == 2){
+    } else if (currentPlayer == 2) {
         currentPlayer = 1;
         score1.innerHTML = `<span style='color: red;'>${p1name}: ${p1score}</span>`;
         score2.innerHTML = `${p2name}: ${p2score}`
